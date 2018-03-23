7129f51ad8a22f612ad34493ecdc381f2bf4e09f
codeTony22 <jgonz275@fiu.edu>
2018-03-23 11:55:50 -0400

    Fixed whitespace in the commit.

--- webapp/geek_text/urls.py
+++ webapp/geek_text/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from .views import signup
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    url(r'^registration/$', signup, name='signup'),
     url(r'^browse/', include('browse.urls', namespace='browse')),
     url(r'^books/', include('book_details.urls', namespace='books')),
     url(r'^reviews/', include('reviews.urls', namespace='reviews')),
