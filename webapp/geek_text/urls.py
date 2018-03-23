dadceeb85b5430cfe7a4099c6def7d10e166f2b4
codeTony22 <jgonz275@fiu.edu>
2018-03-23 11:59:24 -0400

    Fixed whitespace in previous PR for feature/registration

--- webapp/geek_text/urls.py
+++ webapp/geek_text/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from .views import signup
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
