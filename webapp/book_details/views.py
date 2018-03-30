e5b01023f04bb6697b1bfa92849dac8b8f7eee31
Dan Herrera <Daniel_Herrera@ultimatesoftware.com>
2018-03-30 15:23:26 -0400

    Consolidate reviews view into details view

--- webapp/book_details/views.py
+++ webapp/book_details/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.db.models import Avg
 
-from .models import Book
+from .models import Book, Review
+from .forms import ReviewForm
 
 def details(request, book_id):
     book = None
@@ -10,4 +12,23 @@ def details(request, book_id):
     except Book.DoesNotExist:
         pass
 
-    return render(request, 'book_details/details.html', {'book': book})
+    reviews = Book.objects.get(book=book)
+    average_rating = reviews.aggregate(Avg('rating'))['rating__avg']
+
+    if request.user.is_authenticated:
+        try:
+            review = Review.objects.get(book=book, user=request.user)
+        except Review.DoesNotExist:
+            review = Review(book=book, user=request.user)
+    else:
+        review = None
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            form.save()
+    else:
+        form = ReviewForm(instance=review)
+
+    return render(request, 'book_details/details.html',
+            {'book': book, 'reviews': reviews, 'form': form})
