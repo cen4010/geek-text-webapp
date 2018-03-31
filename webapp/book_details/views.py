9f41660916b634dcec5186cfbbe2b1665c231b03
Dan Herrera <Daniel_Herrera@ultimatesoftware.com>
2018-03-31 13:11:45 -0400

    Fix up references and typos from merge

--- webapp/book_details/views.py
+++ webapp/book_details/views.py
@@ -6,14 +6,16 @@ from .forms import ReviewForm
 
 def details(request, book_id):
     book = None
+    reviews = None
+    average_rating = None
 
     try:
         book = Book.objects.get(id=book_id)
+        reviews = Review.objects.filter(book=book)
+        average_rating = reviews.aggregate(Avg('rating'))['rating__avg']
     except Book.DoesNotExist:
         pass
 
-    reviews = Book.objects.get(book=book)
-    average_rating = reviews.aggregate(Avg('rating'))['rating__avg']
 
     if request.user.is_authenticated:
         try:
