975c83c6cc3ebdeb3f0bf72a4b6d452b05eb15de
ogonz110 <ogonz110@fiu.edu>
2018-04-05 00:32:44 -0400

    improve rating stars

--- webapp/book_details/templatetags/review_filters.py
+++ webapp/book_details/templatetags/review_filters.py
@@ -1,5 +1,6 @@
 from django import template
 from django.db.models import Avg
+from django.utils.safestring import mark_safe
 
 from book_details.models import Review
 
@@ -7,22 +8,27 @@ register = template.Library()
 
 @register.filter
 def get_stars(rating):
-    stars = {
-        1: '★☆☆☆☆',
-        2: '★★☆☆☆',
-        3: '★★★☆☆',
-        4: '★★★★☆',
-        5: '★★★★★'
-    }
-
     if rating is None:
-        return None
+        return 'No rating'
 
-    return stars.get(round(rating))
-
+    return mark_safe(('<div class="rating">'
+        '<div class="empty-stars">'
+        '<span class="glyphicon glyphicon-star-empty"></span>'
+        '<span class="glyphicon glyphicon-star-empty"></span>'
+        '<span class="glyphicon glyphicon-star-empty"></span>'
+        '<span class="glyphicon glyphicon-star-empty"></span>'
+        '<span class="glyphicon glyphicon-star-empty"></span>'
+        '</div>'
+        '<div class="full-stars" style="width:{}%;">'
+        '<span class="glyphicon glyphicon-star"></span>'
+        '<span class="glyphicon glyphicon-star"></span>'
+        '<span class="glyphicon glyphicon-star"></span>'
+        '<span class="glyphicon glyphicon-star"></span>'
+        '<span class="glyphicon glyphicon-star"></span>'
+        '</div>'
+        '</div>').format(rating / 5 * 100))
 
 @register.filter
 def avg_rating(book):
     reviews = Review.objects.filter(book=book)
     return get_stars(reviews.aggregate(Avg('rating'))['rating__avg'])
-
