bc7988ee7981d3d4a1cf451a1c8da4a45581e596
Frank Hernandez <fhern077@fiu.edu>
2018-04-06 16:14:56 -0400

    fixed prices to come from book, added multi item support

--- webapp/carts/urls.py
+++ webapp/carts/urls.py
@@ -3,12 +3,14 @@ from django.conf.urls import url
 from .views import (
         cart_home, 
         cart_add_book,
-        cart_remove_book
+        cart_remove_book,
+        cart_update_quantity
         )
 
 urlpatterns = [
     url(r'^$', cart_home, name='home'),
     url(r'^add/$', cart_add_book , name='add'),
     url(r'^remove/$', cart_remove_book , name='remove'),
+    url(r'^update_quantity/$', cart_update_quantity, name='update_quantity'),
 ]
 
