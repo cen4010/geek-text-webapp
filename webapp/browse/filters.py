1fd77fb3f2b83f91b2f4ab357c4e07b522429ab9
Nico Gonzalez <nicogonzalez2012@gmail.com>
2018-04-06 21:32:59 -0400

    Adds filter and sort by rating

--- webapp/browse/filters.py
+++ webapp/browse/filters.py
@@ -1,9 +1,22 @@
-from book_details.models import Book, Author, Publisher, Genre, Review
+from book_details.models import Book, Author, Publisher, Genre
 import django_filters
 from django_filters import OrderingFilter
+from django.db.models import Avg
 
 #class to define filters for books, can be expanded to all fields of book
 class bookFilter(django_filters.FilterSet):
+
+    RATING_CHOICES = (
+        (1, '★☆☆☆☆'),
+        (2, '★★☆☆☆'),
+        (3, '★★★☆☆'),
+        (4, '★★★★☆'),
+        (5, '★★★★★')
+    )
+
+    rating = django_filters.ChoiceFilter(name='reviews', lookup_expr='gte', method='avg_rating', choices=RATING_CHOICES)
+    def avg_rating(self,queryset,name,value):
+        return queryset.annotate(avg_rating=Avg('reviews__rating')).filter(avg_rating=value)
 
     o = OrderingFilter(
         # tuple-mapping retains order
@@ -11,16 +24,10 @@ class bookFilter(django_filters.FilterSet):
             ('title', 'Book Title'),
             ('author', 'Author'),
             ('publish_date', 'Release Date'),
+            ('reviews__rating', 'Average Rating'),
         ),
     )
 
     class Meta:
         model = Book
-        fields = ['title', 'author', 'genre']
-
-
-#class ratingFilter(django_filters.FilterSet):
-
-#    class Meta:
-#        model = Review
-#        fields = ['comment' ]
+        fields = ['title', 'author', 'genre', 'rating']
